0b0f596102e577a43f74d271710c9ea45382b3ef
alanarturoo <alanarturomartinezest@gmail.com>
2024-11-23 18:35:08 -0600

    Practica DesApp

---
+++ app/src/main/java/com/example/navicomponent/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.navicomponent;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configuración para pantalla completa y ocultar la barra de navegación
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Para dispositivos con Android 11 (API 30) y superior
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            // Para dispositivos con versiones anteriores a Android 11
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
